7298ee39161d009c7b6eef7db292396dbbe6f520
Alex Selby <alex.selby@hotmail.com>
2021-12-17 08:51:30 +0000

    add event delegation to improve page nav

--- script.js
+++ script.js
@@ -39,11 +39,11 @@ btnScrollTo.addEventListener("click", function () {
   section1.scrollIntoView({ behavior: "smooth" });
 });
 
-document.querySelectorAll(".nav__link").forEach(function (el) {
-  el.addEventListener("click", function (e) {
-    e.preventDefault();
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
 
-    const id = this.getAttribute("href");
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
-  });
+  }
 });
