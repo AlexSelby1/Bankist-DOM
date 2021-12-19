b167d4ec0e9fec807b1b2b7b4c07bdd0cca84abd
Alex Selby <alex.selby@hotmail.com>
2021-12-19 18:45:45 +0000

    revealing elements on scroll using intersection observer

--- script.js
+++ script.js
@@ -97,10 +97,27 @@ const stickyNav = function (entries) {
   if (!entry.isIntersecting) nav.classList.add("sticky");
   else nav.classList.remove("sticky");
 };
-
+const navHeight = nav.getBoundingClientRect().height;
 const headerObserver = new IntersectionObserver(stickyNav, {
   root: null,
   threshold: 0,
-  rootMargin: "-90px",
+  rootMargin: `-${navHeight}px`,
 });
 headerObserver.observe(header);
+
+// Reveal sections
+const allSections = document.querySelectorAll(".section");
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
